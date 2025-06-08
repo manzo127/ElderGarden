898e9374bf28e148135ee87661774738cdf3862d
manzo127 <lautaro.ordenes@gmail.com>
2025-06-08 18:04:39 -0300

    Initial commit

---
+++ src/main/java/com/elder/garden/Eldergardenclient.java
@@ -0,0 +1,10 @@
+package com.elder.garden;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Eldergardenclient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
