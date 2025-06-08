fd60cc7d1f7c725613f891cb69e2d10d4f929007
manzo127 <lautaro.ordenes@gmail.com>
2025-06-08 18:04:39 -0300

    Initial commit

---
+++ src/main/java/com/elder/garden/ElderGarden.java
@@ -0,0 +1,16 @@
+package com.elder.garden;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ElderGarden implements ModInitializer {
+	public static final String MOD_ID = "elder-garden";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/com/elder/garden/ElderGardenDataGenerator.java
@@ -0,0 +1,11 @@
+package com.elder.garden;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ElderGardenDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
